4e43f5b7aa642f912a6d43f17c9d7dc19a1f8d1a
Nicolas Bezerra <nicolasbezerra14@gmail.com>
2015-07-08 22:57:47 -0300

    Pronto!
    - Quando clica no game pad os outros assests em volta aparecem
    - quando deixa de clicar os assests dessaparecem
    - proporção do game pad diminui quando é pressionado e volta ao normal
    quando é largado.
    - quando se pressiona o game pad e arrasta o mouse e o solta em cima de
    algum outro asset uma scene se abre, como se fosse o jogo que vc
    escolheu

---
+++  Menu Animado/Assets/scripts/clickNewButtons.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class clickNewButtons : MonoBehaviour {
+
+	public int num;
+	public float scaleX;
+	public float scaleY;
+	public float scaleZ;
+	public float Sub;
+	public int newScene;
+	public float Control;
+	private GameObject privatizator;
+
+	void OnMouseDrag(){
+		transform.localScale = new Vector3(scaleX-Sub,scaleY-Sub,scaleZ-Sub);
+		Control = 1;
+		privatizator.SetActive(true);
+        
+	}
+
+	void OnMouseUp(){		
+		transform.localScale = new Vector3 (scaleX + Sub, scaleY + Sub, scaleZ + Sub);
+		Control = 2;
+		privatizator.SetActive (false);
+	
+	}
+
+	void Start () {
+		scaleX = transform.localScale.x;
+		scaleY = transform.localScale.y;
+		scaleZ = transform.localScale.z;
+		Sub = transform.localScale.x/20;
+		privatizator = GameObject.FindGameObjectWithTag("excroto"); 
+		privatizator.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Control == 1)
+        {
+            Vector2 mp = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x,Input.mousePosition.y));
+			if (GameObject.FindGameObjectWithTag("CaminhoRed").GetComponent<BoxCollider2D>().OverlapPoint(mp)) 
+            {
+				newScene = 1;
+
+            }
+			else if (GameObject.FindGameObjectWithTag("CaminhoAmarelo").GetComponent<CircleCollider2D>().OverlapPoint(mp)) 
+			{
+				newScene = 2;
+			}
+			else if(GameObject.FindGameObjectWithTag("CaminhoGreen").GetComponent<BoxCollider2D>().OverlapPoint(mp))
+			{
+				newScene = 3;
+			}
+			else if (GameObject.FindGameObjectWithTag("CaminhoBlue").GetComponent<BoxCollider2D>().OverlapPoint(mp)) 
+			{
+				newScene = 4;
+			}
+        }
+		print (newScene);
+		print(Control);
+
+		if (Control == 2) {
+
+			if(newScene == 1){
+				print ("foi");
+				Application.LoadLevel(1);
+			}
+			if(newScene == 2){
+				print ("foi");
+				Application.LoadLevel(1);
+			}
+			if(newScene == 3){
+				print ("foi");
+				Application.LoadLevel(1);
+			}
+			if(newScene == 4){
+				print ("foi");
+				Application.LoadLevel(1);
+			}
+
+		}
+	}
+}
